1f4af8de879c95213b5f1064f61863f0896d2fce
Nikita Solanki <nikita.solanki@startappss.com>
2026-06-29 10:36:13 +0530

    update :-cors config

--- backend/server.js
+++ backend/server.js
@@ -17,46 +17,50 @@ connectDB();
 const app = express();
 
 const allowedOrigins = [
+  "https://talk-a-tive-hzrsd02oo-nikitasolanki-5851s-projects.vercel.app",
   "http://localhost:5173",
   "https://talk-a-tive-hzrsd02oo-nikitasolanki-5851s-projects.vercel.app/",
 ];
 
 // ---------------- CORS FIX ----------------
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
 
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   }
 
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, OPTIONS"
+//   );
 
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization"
+//   );
 
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(204);
+//   }
 
-  next();
-});
+//   next();
+// });
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 // ---------------- ROUTES ----------------
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
-
+app.use(cors({
+  origin : allowedOrigins,
+methods : ['GET', 'POST', 'PATCH', 'DELETE', 'PUT']
+}))
 app.use("/api/user", userRouter);
 app.use("/api/chat", chatRouter);
 app.use("/api/message", messageRoutes);
